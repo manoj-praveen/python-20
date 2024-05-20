2a27be5837630eaa7aadaabb6533af167b4d7976
manoj-praveen <manojpraveenkgm@gmail.com>
2024-05-20 19:05:58 +0530

    Create mac_touch_id_support.py

---
+++ mac_touch_id_support.py
@@ -0,0 +1,18 @@
+"""
+Code to use touch id verification in python
+Github package: https://github.com/lukaskollmer/python-touch-id
+Command to install -> pip install git+https://github.com/lukaskollmer/python-touch-id.git
+
+"""
+import touchid
+
+
+def main() -> None:
+  print(touchid.is_available()) # to check whether the system has touch id support or not
+  is_verified = touchid.authenticate(reason='authenticate via Touch ID')  
+  # returns true / false based on the finger print entred and the fingerprints added in mac
+  print(is_verified)
+
+
+if __name__ == '__main__':
+  main()
